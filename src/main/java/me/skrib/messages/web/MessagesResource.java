a13ce80fc8d1dc3646e38ec405e335086112b6ab
Damien Rubio <rubio.damien@gmail.com>
2019-10-14 16:44:22 +0200

    allow crossorigin

--- src/main/java/me/skrib/messages/web/MessagesResource.java
+++ src/main/java/me/skrib/messages/web/MessagesResource.java
@@ -6,6 +6,7 @@ import me.skrib.messages.service.MessageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("messages")
 public class MessagesResource {
